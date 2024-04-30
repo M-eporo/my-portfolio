ef74460a0b1986bd61cece47752d48b2400f3ade
shuji-sugishita <gshuji1021@gmail.com>
2024-04-30 23:20:24 +0900

    sixth commit

--- js/menu-slider.js
+++ js/menu-slider.js
@@ -1,11 +1,11 @@
 $(function() {
-	$('.hamburger-btn').on('click', function() {
-		$(this).toggleClass('open');
-		$(".hamburger-btn-area").toggleClass('open');
+  $('.hamburger-btn').on('click', function() {
+    $(this).toggleClass('open');
+    $(".hamburger-btn-area").toggleClass('open');
     if($(".mask").hasClass("open")) {
       $(".mask").removeClass("open").addClass("close");
     }else {
       $(".mask").removeClass("close").addClass("open");
     }
-	});
+  });
 });
